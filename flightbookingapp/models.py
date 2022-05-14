b406aaef77dbeb1e7a0fd244a2e9e4c555154c36
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-14 21:17:15 +0300

    working app

--- flightbookingapp/models.py
+++ flightbookingapp/models.py
@@ -6,6 +6,3 @@ class Student(db.Model):
     studId = db.Column(db.CHAR(6), primary_key=True, nullable=False)
     name = db.Column(db.VARCHAR(50), nullable=False)
     email = db.Column(db.VARCHAR(50), nullable=False)
-
-
-db.create_all()
