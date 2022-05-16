6e1895ced7588653c97616f4327d10b94b97d27c
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-16 20:58:40 +0300

    change database pathname

--- flightbookingapp/__init__.py
+++ flightbookingapp/__init__.py
@@ -6,8 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir, 'flightbookingapp/main.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "c0aa993116025ec489283c5bb9f4f059"  # TODO env variable
 
