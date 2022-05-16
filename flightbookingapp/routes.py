cc129cb9bc83ae4403821618218c0836700578ad
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-05-16 23:15:28 +0300

    add redirection on login

--- flightbookingapp/routes.py
+++ flightbookingapp/routes.py
@@ -1,7 +1,7 @@
 from flightbookingapp import app, db, bcrypt
 from flightbookingapp.forms import *
 from flightbookingapp.models import Aircraft, Customer, Route, Airport, Booking, Departure
-from flask import render_template, jsonify, request, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -22,11 +22,12 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        customer = Customer.query.filter_by(email=form.email.data).first()
-        if customer and bcrypt.check_password_hash(customer.password, form.password.data):
-            login_user(customer, remember=form.remember.data)
-            flash(f"Welcome back to Kulta Air, {customer.first_name}.", 'success')
-            return redirect(url_for('home'))
+        customer_to_login = Customer.query.filter_by(email=form.email.data).first()
+        if customer_to_login and bcrypt.check_password_hash(customer_to_login.password, form.password.data):
+            login_user(customer_to_login, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash(f"Welcome back to Kulta Air, {customer_to_login.first_name}.", 'success')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash(f"Username or password invalid. Check your information and try again.", 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -39,11 +40,11 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        customer = Customer(first_name=form.firstname.data,
-                            last_name=form.lastname.data,
-                            email=form.email.data,
-                            password=hashed_password)
-        db.session.add(customer)
+        new_customer = Customer(first_name=form.firstname.data,
+                                last_name=form.lastname.data,
+                                email=form.email.data,
+                                password=hashed_password)
+        db.session.add(new_customer)
         db.session.commit()
         flash(f"Welcome to Kulta Air, {form.firstname.data}. You can now login.", 'success')
         return redirect(url_for('login'))
