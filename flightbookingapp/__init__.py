5f9a6bdc546230416070f5bb90fa2dc940c4dc9d
Josh Pearson <36744690+baconeta@users.noreply.github.com>
2022-06-23 10:19:51 +0300

    slight tidy up

--- flightbookingapp/__init__.py
+++ flightbookingapp/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "c0aa993116025ec489283c5bb9f4f059"  # TODO env variable
+app.config['SECRET_KEY'] = "c0aa993116025ec489283c5bb9f4f059"  # Set with environment variables in production
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
